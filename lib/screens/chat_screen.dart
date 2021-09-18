b744ba944398a52d174116829004f74e98f359bc
ixoye215 <ixoye215@gmail.com>
2021-09-18 16:05:00 -0500

    Fin seccion 7

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,158 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ChatScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ixoye_chat/widgets/chat_message.dart';
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({ Key? key }) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin{
+
+  final _textController = new TextEditingController();
+  final _focusNode = new FocusNode();
+
+  List<ChatMessage> _messages = [
+    
+  ];
+
+  bool _estaEscribiendo = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ChatScreen'),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text('Te', style: TextStyle( fontSize: 12),),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+            ),
+            SizedBox( height: 3,),
+            Text('Melissa Flores', style: TextStyle( color: Colors.black87, fontSize: 12),)
+          ],
+        ),
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemCount: _messages.length,
+                itemBuilder: (_, i) => _messages[i],
+                physics: BouncingScrollPhysics(),
+                reverse: true,
+              )
+            ),
+            Divider( height: 1,),
+            Container(
+              color: Colors.white,
+              child: _inputChat(),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _inputChat(){
+
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child:  TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmit,
+                onChanged: (String texto){
+                  setState(() {
+                    if( texto.trim().length > 0){
+                      _estaEscribiendo = true;
+                    }else{
+                      _estaEscribiendo = false;
+                    }
+                  });
+                },
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Enviar mensaje'
+                ),
+                focusNode: _focusNode,
+              )
+            ),
+
+            //boton de enviar
+            Container(
+              margin: EdgeInsets.symmetric( horizontal: 4.0),
+              child: Platform.isIOS ?
+                CupertinoButton(
+                  child: Text('Enviar'), 
+                  onPressed: (){}
+                )
+                : Container(
+                  margin: EdgeInsets.symmetric( horizontal: 4.0),
+                  child: IconTheme(
+                    data: IconThemeData( color: Colors.blue[400]),
+                    child: IconButton(
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      onPressed: _estaEscribiendo
+                        ? ()=>_handleSubmit(_textController.text)
+                        : null, 
+                      icon: Icon(Icons.send,),
+                    ),
+                  ),
+                )
+
+              ,
+            )
+          ],
+        ),
       ),
     );
+
+
+  }
+
+  _handleSubmit(String texto){
+    if (texto.length == 0) {
+      return;
+    }
+    
+    _textController.clear();
+    _focusNode.requestFocus();
+
+    final newMessage = new ChatMessage(
+      texto: texto,
+      uid: '123',
+      animationController: AnimationController(
+        vsync: this, 
+        duration: Duration(milliseconds: 400)
+      ),
+    );
+    _messages.insert(0, newMessage);
+    newMessage.animationController.forward();
+    setState(() {
+      _estaEscribiendo = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: Off del socket
+
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 }
--- lib/screens/usuarios_screen.dart
+++ lib/screens/usuarios_screen.dart
@@ -1,14 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:ixoye_chat/models/usuario.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class UsuariosScreen extends StatelessWidget {
+class UsuariosScreen extends StatefulWidget {
   const UsuariosScreen({ Key? key }) : super(key: key);
+
+  @override
+  _UsuariosScreenState createState() => _UsuariosScreenState();
+}
+
+class _UsuariosScreenState extends State<UsuariosScreen> {
+
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  final usuarios = [
+    Usuario(uid: '1', nombre: 'Brayan', email: 'test1@test.com', online: true),
+    Usuario(uid: '2', nombre: 'Jefa', email: 'test2@test.com', online: false),
+    Usuario(uid: '3', nombre: 'Luis', email: 'test3@test.com', online: true),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Usuario Screen'),
+      appBar: AppBar(
+        title: Text(
+          'Mi Nombre',
+          style: TextStyle(
+            color: Colors.black87
+          ),
+        ),
+        centerTitle: true,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: (){}, 
+          icon: Icon(Icons.exit_to_app, color: Colors.black87,),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: Icon(Icons.check_circle, color: Colors.red,),
+          )
+        ],
       ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        enablePullDown: true,
+        header: WaterDropHeader(
+          complete: Icon(Icons.check, color: Colors.blue[400],),
+          waterDropColor: Colors.blueAccent,
+        ),
+        onRefresh: _cargarUsuarios,
+        child: _listViewUsuarios(),
+
+      )
     );
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: BouncingScrollPhysics(),
+      itemBuilder: ( _, i )=> _usuarioListTile(usuarios[i]), 
+      separatorBuilder: ( _ , i) => Divider(), 
+      itemCount: usuarios.length
+    );
+  }
+
+  ListTile _usuarioListTile(Usuario usuario) {
+    return ListTile(
+        title: Text(usuario.nombre),
+        subtitle: Text(usuario.email),
+        leading: CircleAvatar(
+          child: Text(usuario.nombre.substring(0,2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        trailing: Container(
+          width: 10.0,
+          height: 10.0,
+          decoration: BoxDecoration(
+            color: usuario.online ? Colors.green[300] : Colors.red,
+            borderRadius: BorderRadius.circular(100.0),
+          ),
+        ),
+      );
+  }
+
+  _cargarUsuarios() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
